5f407f58ae0d00133e28ff4c4fb2d40cc7aa492c
jefsbruno <jefsbruno@soft.tec.br>
2022-10-19 10:03:24 -0300

    Fix: remove  div

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import { GlobalStyle } from "./styles/global";
 function App() {
   return (
     <>
-    <GlobalStyle/>
-      <div>
-        <h1>Olar mundo</h1>
-      </div>
+      <GlobalStyle />
+
+      <h1>Olar mundo</h1>
     </>
   );
 }
